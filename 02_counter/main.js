cbed0dfee2055877ed89743d0fc8a401988c2116
Yoon <s2464@e-mirim.hs.kr>
2026-03-20 10:04:32 +0900

    counter HTML, JS

---
+++ 02_counter/main.js
@@ -0,0 +1,18 @@
+let number = 0;
+
+// h1에 있는 숫자 요소
+// const resultH1= document.getElementsByTagName("h1")[0];
+const resultH1 = document.getElementById("result");
+// button에 있는 + 요소
+// const plusButton = document.getElementsByTagName("button")[0];
+const plusButton = document.getElementsByClassName("plus")[0];
+
+// plusButton.addEventListener("click", () => {
+//     number++;
+//     resultH1.innerHTML = number;
+// });
+
+plusButton.onclick = () => {
+    number++;
+    resultH1.innerHTML = number;
+}
