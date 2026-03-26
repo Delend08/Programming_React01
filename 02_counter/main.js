46aa8d22302df881ffda2a2d4833d8429e07b4ab
Yoon <s2464@e-mirim.hs.kr>
2026-03-27 08:52:54 +0900

    counter HTML 0 삭제, JS number 만 남기자

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,4 +1,5 @@
-let number = 0;
+const baseNumber = 100;
+let number = baseNumber;
 
 // h1에 있는 숫자 요소
 // const resultH1= document.getElementsByTagName("h1")[0];
@@ -17,11 +18,18 @@ const resultH1 = document.getElementById("result");
 //     resultH1.innerHTML = number;
 // }
 
-const changeNumber = (n) => {
-    number += n;
+const render = () => {
     resultH1.innerHTML = number;
 }
 
-const reset = () => {
-    resultH1.innerHTML = 0;
+const changeNumber = (n = 1) => {
+    number += n;
+    render();
 }
+
+const reset = () => {
+    number = baseNumber;
+    render();
+}
+
+render();
