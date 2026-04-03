56196c10a84c93419e31f15ba93dacffab58588f
Yoon <s2464@e-mirim.hs.kr>
2026-04-03 09:40:17 +0900

    CounterApp +2, -, reset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,13 +5,18 @@ import heroImg from './assets/hero.png'
 // import './App.css'
 
 function CounterApp() {
+  const baseNumber = 0
   //count: 현재 또는 이전 상태 값; serCount(): 상태값 변경 함수
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(baseNumber)
+  const numberConversion = (n) => setCount(count => count + n);
+  const reset = () => setCount(baseNumber)
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count => count+1)}>+</button>
+      <button onClick={() => numberConversion(1)}>+</button>
+      <button onClick={() => numberConversion(2)}>+2</button>
+      <button onClick={() => numberConversion(-1)}>-</button>
+      <button onClick={() => reset()}>reset</button>
       {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}
