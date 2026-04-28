1d23b2fb39bc670d37e61090f4e8316f7c72a84c
Yoon <s2464@e-mirim.hs.kr>
2026-04-29 08:40:16 +0900

    TodoListApp editTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,9 +1,9 @@
-import './todolist.css'
 import TodoHeader from './components/TodoHeader.jsx';
 import TodoAdder from './components/TodoAdder.jsx';
 import TodoList from './components/TodoList.jsx';
 import { useState } from 'react';
 import { Todo } from './type/Todo.jsx';
+import './todolist.css'
 
 function TodoListApp() {
     const [todos, setTodos] = useState([]);
@@ -23,11 +23,17 @@ function TodoListApp() {
             todos.filter((todo) => todo.id !== id)
         )
     }
+
+    const editTodo = (id, newText) => {
+        setTodos((todos) => 
+            todos.map((todo) => todo.id=== id ? { ...todo, text:newText} : todo)
+        )
+    }
     return (
         <div className="todo">
             <TodoHeader />
             <TodoAdder addTodo={addTodo} />
-            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} editTodo={editTodo}/>
         </div>
     )
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import CheckBox from './CheckBox.jsx'
 import Button from './Button.jsx'
 
-export default function TodoItem({ todo, toggleTodo, deleteTodo }) {
+export default function TodoItem({ todo, toggleTodo, deleteTodo, editTodo }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(todo.text);
 
+    function handleEdit() {
+        if (!isEditing) {
+            setIsEditing(true)
+            setEditText(todo.text);
+        } else {
+            const trimmedText = editText.trim();
+            if (trimmedText !== todo.text && trimmedText !== "") {
+                editTodo(todo.id ,trimmedText);
+            }
+            setIsEditing(false);
+        }
+    }
+    function handleEnter(e) {
+        e.key === 'Enter' && handleEdit()
+    }
     return (
         <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
-            <CheckBox 
-                id={todo.id}
-                checked={todo.isCompleted}
-                onChange={() => toggleTodo(todo.id)}    
-            >
-                {todo.text}
-            </CheckBox>
-            <Button className="todo__Button todo__Button--edit">✏️</Button>
+
+            {
+                !isEditing &&
+                <CheckBox
+                    id={todo.id}
+                    checked={todo.isCompleted}
+                    onChange={() => toggleTodo(todo.id)}
+                >
+                    {todo.text}
+                </CheckBox>
+            }
+            {
+                isEditing &&
+                <input
+                    type='text'
+                    className='todo__input--edit'
+                    value={editText}
+                    onChange={(event) => setEditText(event.target.value)}
+                    autoFocus
+                    onKeyUp={handleEnter}
+                />
+            }
+            <Button className="todo__Button todo__Button--edit" onClick={handleEdit}>✏️</Button>
             <Button className="todo__Button todo__Button--delete" onClick={() => deleteTodo(todo.id)}>❌</Button>
         </li>
     )
